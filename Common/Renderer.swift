3597b47b2069a11ca865ec32cbabaf9db0370de5
Scott <scott.mehus@possible.com>
2019-01-27 08:17:07 -0700

    Shadow depth texture showing correctly

--- Common/Renderer.swift
+++ Common/Renderer.swift
@@ -160,12 +160,11 @@ extension Renderer: MTKViewDelegate {
 
         renderEncoder.setDepthBias(0.01, slopeScale: 1.0, clamp: 0.01)
 
-        let rect = Rectangle(left: -20, right: 20, top: 20, bottom: -20)
-        scene.uniforms.projectionMatrix = float4x4(orthographic: rect, near: 0.1, far: 300)
+        let rect = Rectangle(left: -8, right: 8, top: 8, bottom: -8)
+        scene.uniforms.projectionMatrix = float4x4(orthographic: rect, near: 0.1, far: 16)
 
         let sunlight = scene.lights.first!
         let position: float3 = [-sunlight.position.x, -sunlight.position.y, -sunlight.position.z]
-
         let center: float3 = [0, 0, 0]
         let lookAt = float4x4(eye: position, center: center, up: [0,1,0])
 
