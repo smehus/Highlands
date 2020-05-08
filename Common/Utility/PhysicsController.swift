545993f3994c8dd72442dafaa2a6139e36fd451b
Scott <scott.mehus@possible.com>
2020-05-07 18:28:20 -0600

    refactoring collision

--- Common/Utility/PhysicsController.swift
+++ Common/Utility/PhysicsController.swift
@@ -31,19 +31,35 @@ class PhysicsController {
         let nodeRadius = max((node.size.x / 2), (node.size.z / 2))
         // This calculates the flaot3 position of the node
         let nodePosition = node.worldTransform.columns.3.xyz
-        
+
+        //
+
+
+        func t(iteratedTransform: Transform, n1Position: SIMD3<Float>, n1Radius: Float, n2Radius: Float) -> Bool {
+            let bodyPosition = iteratedTransform.modelMatrix.columns.3.xyz
+            let d = distance(n1Position, bodyPosition)
+            if d < (n1Radius + n2Radius) {
+                if holdAllCollided {
+                    return true
+                } else {
+                    return false
+                }
+            }
+
+            return false
+        }
+
+        /// Handling collisions with player
         for body in staticBodies  {
             let bodyRadius = max((body.size.x / 2), (body.size.z / 2))
             if let prop = body as? Prop, prop.propType.isInstanced {
                 for transform in prop.transforms {
-                    let bodyPosition = transform.modelMatrix.columns.3.xyz
-                    let d = distance(nodePosition, bodyPosition)
-                    if d < (nodeRadius + bodyRadius) {
-                        if holdAllCollided {
-                            collidedBodies.append(transform)
-                        } else {
-                            return []
-                        }
+                    if t(iteratedTransform: transform,
+                         n1Position: nodePosition,
+                         n1Radius: nodeRadius,
+                         n2Radius: bodyRadius) {
+
+                        collidedBodies.append(transform)
                     }
                 }
             } else {
